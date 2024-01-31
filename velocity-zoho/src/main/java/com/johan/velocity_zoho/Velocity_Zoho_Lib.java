8ec4075e239cc5db86880382623c9f149d21153e
V3JohanM <124780665+V3JohanM@users.noreply.github.com>
2024-01-31 13:38:12 +0200

    Initial commit

---
+++ velocity-zoho/src/main/java/com/johan/velocity_zoho/Velocity_Zoho_Lib.java
@@ -0,0 +1,7 @@
+package com.johan.velocity_zoho;
+
+public class Velocity_Zoho_Lib {
+    public static String message() {
+        return "The String is returned by using Return Statement";
+    }
+}
